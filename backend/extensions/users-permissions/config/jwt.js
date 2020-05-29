5281f001aa204f1a986d72f5650f924e0241e475
pooja kushwaha <kushwahapooja158@gmail.com>
2020-05-29 14:23:47 +0530

    blog

---
+++ backend/extensions/users-permissions/config/jwt.js
@@ -0,0 +1,3 @@
+module.exports = {
+  jwtSecret: process.env.JWT_SECRET || '5dbcff41-ad3a-4d6d-9c6c-60f0e0f3dfb0'
+};
---
+++ frontend/src/utils/apolloClient.js
@@ -0,0 +1,23 @@
+
+import { ApolloClient } from "apollo-client";
+import { InMemoryCache } from "apollo-cache-inmemory";
+import { HttpLink } from "apollo-link-http";
+
+const cache = new InMemoryCache();
+const link = new HttpLink({
+  uri: `${process.env.REACT_APP_BACKEND_URL}/graphql`||
+  "http://localhost:1337"
+});
+const client = new ApolloClient({
+  cache,
+  link
+});
+
+
+
+
+
+export default client;
+
+
+
